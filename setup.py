2ce2b5d7a61b4cc6f573085386dd175092c9860d
Luke Hodkinson <furious.luke@gmail.com>
2016-06-09 10:55:40 +1000

    First commit.

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os, sys
+from setuptools import setup, find_packages
+
+setup(
+    name='django-xauth',
+    version='0.1.0',
+    author='Luke Hodkinson',
+    author_email='furious.luke@gmail.com',
+    maintainer='Luke Hodkinson',
+    maintainer_email='furious.luke@gmail.com',
+    url='https://github.com/furious-luke/django-xauth',
+    description='A simple Django application for AJAX authorisation.',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['*.txt', '*.js', '*.html', '*.*']},
+    install_requires=['setuptools'],
+    zip_safe=False,
+)
