ebd7a68fdd3318ca7f8f4f1b86b0c9d71f0b933b
Luke Hodkinson <furious.luke@gmail.com>
2017-11-01 10:56:43 +1100

    Need to use a different naming convention

--- xauth/login_ajax_urls.py
+++ xauth/login_ajax_urls.py
@@ -7,6 +7,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', views.login, name='login'),
-    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^login/$', views.login, name='ajax_login'),
+    url(r'^logout/$', views.logout, name='ajax_logout'),
 ]
